2bce10c4c3080ede8a06027bca1047fd1f1e3a26
dahniardd <dahniardvn09@gmail.com>
2024-02-28 15:53:14 +0700

    Contoh For  Each

---
+++ ContohForeach06.java
@@ -0,0 +1,18 @@
+public class ContohForeach06 {
+
+        public static void main(String[] args) {
+            int[] daftarNilai = new int[5];
+            daftarNilai[0] = 7;
+            daftarNilai[1] = 3;
+            daftarNilai[2] = 8;
+            daftarNilai[3] = 2;
+            daftarNilai[4] = 1;
+    
+            // Looping menggunakan for-each
+            for (int nilai : daftarNilai) {
+                System.out.println(nilai);
+            }
+        }
+}
+    
+
