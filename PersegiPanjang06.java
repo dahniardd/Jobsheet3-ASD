c5583da3e477108b282d172393769affadf38ac1
dahniardd <dahniardvn09@gmail.com>
2024-03-01 22:45:25 +0700

    PersegiPanjangDemo06

--- PersegiPanjang06.java
+++ PersegiPanjang06.java
@@ -1,26 +1,20 @@
 public class PersegiPanjang06 {
     public int panjang;
     public int lebar;
-public static void main(String[] args) {
-    PersegiPanjang06[] arrayOfPersegiPanjang = new PersegiPanjang06[3];
 
-    arrayOfPersegiPanjang[0] = new PersegiPanjang06();
-    arrayOfPersegiPanjang[0].panjang = 110;
-    arrayOfPersegiPanjang[0].lebar = 30;
-
-    arrayOfPersegiPanjang[1] = new PersegiPanjang06();
-    arrayOfPersegiPanjang[1].panjang = 80;
-    arrayOfPersegiPanjang[1].lebar = 40;
-
-    arrayOfPersegiPanjang[2] = new PersegiPanjang06();
-    arrayOfPersegiPanjang[2].panjang = 100;
-    arrayOfPersegiPanjang[2].lebar = 20;
-
-    System.out.println("Persegi panjang ke-1, panjang : " + arrayOfPersegiPanjang[0].panjang + ", lebar: " 
-    + arrayOfPersegiPanjang[0].lebar);
-    System.out.println("Persegi panjang ke-2, panjang : " + arrayOfPersegiPanjang[1].panjang + ", lebar: " 
-    + arrayOfPersegiPanjang[1].lebar);
-    System.out.println("Persegi panjang ke-3, panjang : " + arrayOfPersegiPanjang[2].panjang + ", lebar: " 
-    + arrayOfPersegiPanjang[2].lebar);
-}
+    public void cetakInfo() {
+    }
+    public PersegiPanjang06(int p, int l) {
+        panjang = p;
+        lebar = l;
+    }
+    public PersegiPanjang06() {
+        //TODO Auto-generated constructor stub
+    }
+    public int hitungLuas() {
+        return panjang*lebar;
+    }
+    public int hitungKeliling() {
+        return 2*(panjang*lebar);
+    } 
 }
